e8e0f8d26b9d83344028b4e05d4bd806b1183544
Matthieu <matthieupasserel@gmail.com>
2018-02-26 14:52:47 +0100

    Classe Plat

---
+++ MonRestoDuMonde/Modele/Enums.swift
@@ -0,0 +1,16 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PASSEREL on 26/02/2018.
+//  Copyright © 2018 Matthieu PASSEREL. All rights reserved.
+//
+
+import Foundation
+
+
+enum Type {
+    case entree
+    case platPrincipal
+    case dessert
+}
