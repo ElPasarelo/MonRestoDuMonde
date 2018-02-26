d028033a9491e0bce39639dd881a7530897c97de
Matthieu <matthieupasserel@gmail.com>
2018-02-26 14:20:47 +0100

    Mon Navigation Controller

---
+++ MonRestoDuMonde/Controlleur/MonNav.swift
@@ -0,0 +1,19 @@
+//
+//  MonNav.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PASSEREL on 26/02/2018.
+//  Copyright © 2018 Matthieu PASSEREL. All rights reserved.
+//
+
+import UIKit
+
+class MonNav: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.barTintColor = NOIR
+        navigationBar.tintColor = GRIS_TRES_CLAIR
+        navigationBar.titleTextAttributes = [.font: FONT_DE_BASE as Any, .foregroundColor: GRIS_TRES_CLAIR]
+    }
+}
