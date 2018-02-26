f2182a289e27547d07961c88fdfda50365ffa548
Matthieu <matthieupasserel@gmail.com>
2018-02-26 15:06:13 +0100

    Menu

---
+++ MonRestoDuMonde/Modele/Menu.swift
@@ -0,0 +1,35 @@
+//
+//  Menu.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PASSEREL on 26/02/2018.
+//  Copyright © 2018 Matthieu PASSEREL. All rights reserved.
+//
+
+import UIKit
+
+class Menu {
+    
+    private var _pays: String
+    private var _image: UIImage
+    private var _plats: [Plat]
+    
+    var pays: String {
+        return _pays
+    }
+    
+    var image: UIImage {
+        return _image
+    }
+    
+    var plats: [Plat] {
+        return _plats
+    }
+    
+    init(pays: String, image: UIImage) {
+        self._pays = pays
+        self._image = image
+        self._plats = [Plat]()
+    }
+    
+}
