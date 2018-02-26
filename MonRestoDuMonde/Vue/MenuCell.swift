3690ca59c1e44901f0f3207cc90f8436c54349b2
Matthieu <matthieupasserel@gmail.com>
2018-02-26 14:45:42 +0100

    Ajout CollectionView

---
+++ MonRestoDuMonde/Vue/MenuCell.swift
@@ -0,0 +1,15 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PASSEREL on 26/02/2018.
+//  Copyright © 2018 Matthieu PASSEREL. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+    
+    
+}
