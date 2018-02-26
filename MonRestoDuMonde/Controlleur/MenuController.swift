5dca207bb7883f11d1fc89344293870ed3e5de9e
Matthieu <matthieupasserel@gmail.com>
2018-02-26 14:02:05 +0100

    Ajout des controlleurs principaux

---
+++ MonRestoDuMonde/Controlleur/MenuController.swift
@@ -0,0 +1,16 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by Matthieu PASSEREL on 26/02/2018.
+//  Copyright © 2018 Matthieu PASSEREL. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
